8af5d798d6ee5f2c48ec3ce855adf197e1048f7b
Provenoc <65681245+Provenoc@users.noreply.github.com>
2021-07-25 22:44:12 -0400

    Add files via upload
    * Hardware name check for stolen password protection

---
+++ EyeRemember/localization/compile.py
@@ -0,0 +1,31 @@
+import os
+import importlib
+import sys
+import pickle
+import platform
+import pathlib
+filepath = os.getcwd()
+def start():
+    
+
+def RequestLogin():
+    askedinput = input("Create a Password: ")
+    if askedinput == "":
+        RequestLogin()
+    else:
+        hardware_name = platform.node()
+        data = [askedinput,hardware_name]
+        filenew = open(filepath+"/"+"localization/"+"data/Pass.data",'ab')
+        pickle.dump(data,filenew)
+
+
+
+try:
+    testfile = open(filepath+"/"+"localization/"+"data/Pass.data",'rb')
+    TestLoad = pickle.load(testfile)
+    
+except:
+    RequestLogin()
+    
+
+
